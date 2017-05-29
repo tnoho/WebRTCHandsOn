285b3b69ae381e39be42218e22d90c7dbe704152
tnoho <teaotya@gmail.com>
2017-05-29 14:37:09 +0900

    WebSocket接続を追加

--- WebRTCHandsOn/ChatViewController.swift
+++ WebRTCHandsOn/ChatViewController.swift
@@ -8,35 +8,55 @@
 
 import UIKit
 import WebRTC
+import Starscream
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, WebSocketDelegate {
+    var websocket: WebSocket! = nil
 
     @IBOutlet weak var localVideoView: RTCEAGLVideoView!
     @IBOutlet weak var remoteVideoView: RTCEAGLVideoView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        websocket = WebSocket(url: URL(string: "wss://conf.space/WebRTCHandsOnSig/tnoho")!)
+        websocket.delegate = self
+        websocket.connect()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func websocketDidConnect(socket: WebSocket) {
+        LOG("websocketが接続されました")
     }
-    */
+    
+    func websocketDidDisconnect(socket: WebSocket, error: NSError?) {
+        LOG("websocketが切断されました: \(String(describing: error?.localizedDescription))")
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocket, text: String) {
+        LOG("Messageを受信しました : \(text)")
+    }
+    
+    func websocketDidReceiveData(socket: WebSocket, data: Data) {
+        LOG("Dataを受信しました : \(data.count)")
+    }
     
     @IBAction func disconnectButtonAction(_ sender: Any) {
+        // 切断ボタンを押した時
+        websocket.disconnect()
         _ = self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    // 参考にさせていただきました！Thanks: http://seesaakyoto.seesaa.net/article/403680516.html
+    func LOG(_ body: String = "",
+             function: String = #function,
+             line: Int = #line)
+    {
+        print("[\(function) : \(line)] \(body)")
     }
 }
