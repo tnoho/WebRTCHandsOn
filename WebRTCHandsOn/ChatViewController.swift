639884593eff9a9d6ec718363fd0ee6de8743dd1
tnoho <teaotya@gmail.com>
2017-05-29 16:25:10 +0900

    PeerConnectionを追加

--- WebRTCHandsOn/ChatViewController.swift
+++ WebRTCHandsOn/ChatViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 import WebRTC
 import Starscream
 
-class ChatViewController: UIViewController, WebSocketDelegate {
+class ChatViewController: UIViewController, WebSocketDelegate, RTCPeerConnectionDelegate {
     var websocket: WebSocket! = nil
+    
+    var peerConnectionFactory: RTCPeerConnectionFactory! = nil
+    var peerConnection: RTCPeerConnection! = nil
+    var localVideoTrack: RTCVideoTrack?
 
     @IBOutlet weak var localVideoView: RTCEAGLVideoView!
     @IBOutlet weak var remoteVideoView: RTCEAGLVideoView!
@@ -19,10 +23,51 @@ class ChatViewController: UIViewController, WebSocketDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // RTCPeerConnectionFactoryの初期化
+        peerConnectionFactory = RTCPeerConnectionFactory()
+        
         websocket = WebSocket(url: URL(string: "wss://conf.space/WebRTCHandsOnSig/tnoho")!)
         websocket.delegate = self
         websocket.connect()
+    }
+    
+    func createPeerConnection() {
+        // STUN/TURNサーバーの指定
+        let configuration = RTCConfiguration()
+        configuration.iceServers = [
+            RTCIceServer.init(
+                urlStrings: ["stun:stun.l.google.com:19302"])]
+        // PeerConecctionの設定(今回はなし)
+        let peerConnectionConstraints = RTCMediaConstraints(
+            mandatoryConstraints: nil, optionalConstraints: nil)
+        // PeerConnectionの初期化
+        peerConnection = peerConnectionFactory.peerConnection(
+            with: configuration, constraints: peerConnectionConstraints, delegate: self)
+        
+        // 音声ソースの設定
+        let audioSourceConstraints = RTCMediaConstraints(
+            mandatoryConstraints: nil, optionalConstraints: nil)
+        // 音声ソースの生成
+        let audioSource = peerConnectionFactory.audioSource(with: audioSourceConstraints)
+        // 音声トラックの作成
+        let localAudioTrack = peerConnectionFactory.audioTrack(with: audioSource, trackId: "ARDAMSa0")
+        // PeerConnectionからSenderを作成
+        let audioSender = peerConnection.sender(withKind: kRTCMediaStreamTrackKindAudio, streamId: "ARDAMS")
+        // Senderにトラックを設定
+        audioSender.track = localAudioTrack
+        
+        // 映像ソースの設定
+        let videoSourceConstraints = RTCMediaConstraints(
+            mandatoryConstraints: nil, optionalConstraints: nil)
+        let videoSource = peerConnectionFactory.avFoundationVideoSource(with: videoSourceConstraints)
+        // 映像トラックの作成
+        localVideoTrack = peerConnectionFactory.videoTrack(with: videoSource, trackId: "ARDAMSv0")
+        // PeerConnectionからVideoのSenderを作成
+        let videoSender = peerConnection.sender(withKind: kRTCMediaStreamTrackKindVideo, streamId: "ARDAMS")
+        // Senderにトラックを設定
+        videoSender.track = localVideoTrack
+        // トラックに自画像表示枠を追加
+        localVideoTrack?.add(localVideoView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,6 +77,7 @@ class ChatViewController: UIViewController, WebSocketDelegate {
 
     func websocketDidConnect(socket: WebSocket) {
         LOG("websocketが接続されました")
+        createPeerConnection()
     }
     
     func websocketDidDisconnect(socket: WebSocket, error: NSError?) {
@@ -46,9 +92,52 @@ class ChatViewController: UIViewController, WebSocketDelegate {
         LOG("Dataを受信しました : \(data.count)")
     }
     
+    func peerConnection(_ peerConnection: RTCPeerConnection, didChange stateChanged: RTCSignalingState) {
+        // 接続情報交換の状況が変化した際に呼ばれます
+    }
+    
+    func peerConnection(_ peerConnection: RTCPeerConnection, didAdd stream: RTCMediaStream) {
+        // 映像/音声が追加された際に呼ばれます
+    }
+    
+    func peerConnection(_ peerConnection: RTCPeerConnection, didRemove stream: RTCMediaStream) {
+        // 映像/音声削除された際に呼ばれます
+    }
+    
+    func peerConnectionShouldNegotiate(_ peerConnection: RTCPeerConnection) {
+        // 接続情報の交換が必要になった際に呼ばれます
+    }
+    
+    func peerConnection(_ peerConnection: RTCPeerConnection, didChange newState: RTCIceConnectionState) {
+        // PeerConnectionの接続状況が変化した際に呼ばれます
+    }
+    
+    func peerConnection(_ peerConnection: RTCPeerConnection, didChange newState: RTCIceGatheringState) {
+        // 接続先候補の探索状況が変化した際に呼ばれます
+    }
+    
+    func peerConnection(_ peerConnection: RTCPeerConnection, didGenerate candidate: RTCIceCandidate) {
+        // Candidate(自分への接続先候補情報)が生成された際に呼ばれます
+    }
+    
+    func peerConnection(_ peerConnection: RTCPeerConnection, didOpen dataChannel: RTCDataChannel) {
+        // DataChannelが作られた際に呼ばれます
+    }
+    
+    func peerConnection(_ peerConnection: RTCPeerConnection, didRemove candidates: [RTCIceCandidate]) {
+        // Candidateが削除された際に呼ばれます
+    }
+    
+    func releaseAll() {
+        localVideoTrack = nil
+        peerConnection = nil
+        peerConnectionFactory = nil
+    }
+    
     @IBAction func disconnectButtonAction(_ sender: Any) {
         // 切断ボタンを押した時
         websocket.disconnect()
+        releaseAll()
         _ = self.navigationController?.popToRootViewController(animated: true)
     }
     
